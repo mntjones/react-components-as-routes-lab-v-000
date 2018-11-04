e8321e30c8e5e8c4a80b3ee151a22b421973d2d3
Monica Tamaru Jones <mntamaru@gmail.com>
2018-11-04 20:27:22 +0000

    automatically backed up by learn

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,18 +2,19 @@ import React from 'react';
 import { directors } from '../data';
 
 const Directors = () => {
-  return (
+ return (
     <div>
       <h1>Directors Page</h1>
       {directors.map((director, i) => (
         <div key={i}>
           <h3>Name: {director.name}</h3>
-          <p>Movies:</p>
-          <ul>
-            {directors.movies.map((movie,i)=> (
-            <li key={i}>{movie}</li>
-            ))}
-          </ul>
+          <p>Movies:
+            <ul>
+              {director.movies.map((movie, i) => (
+                <li key={i}>{movie}</li>
+              ))}
+            </ul>
+          </p>
         </div>
       ))}
     </div>
