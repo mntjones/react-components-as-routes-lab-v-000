4e9af8565400baf6f1c07909f43ebd6ee130b7d2
Monica Tamaru Jones <mntamaru@gmail.com>
2018-11-04 20:04:07 +0000

    automatically backed up by learn

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,7 @@ import { NavLink } from 'react-router-dom';
 
 const NavBar = () => {
   return (
-    <div className="navbar">
+    <div class="navbar">
       <NavLink
         to="/"
         exact
