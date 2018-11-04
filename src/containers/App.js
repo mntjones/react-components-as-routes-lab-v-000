667b5f53ce1b8681e8a337f64c5616b483a06145
Monica Tamaru Jones <mntamaru@gmail.com>
2018-11-04 19:49:09 +0000

    automatically backed up by learn

--- src/containers/App.js
+++ src/containers/App.js
@@ -16,9 +16,9 @@ const App = (props) => {
       <React.Fragment>
         <Navbar />
         <Route path="/" render={Home} />
-        <Route exact path="/movies" render={Movies} />
-        <Route exact path="/directors" render={Directors} />
-        <Route exact path="/actors" render={Actors} />
+        <Route exact path="/movies" component={Movies} />
+        <Route exact path="/directors" component={Directors} />
+        <Route exact path="/actors" component={Actors} />
       </React.Fragment>
     </Router>
   );
