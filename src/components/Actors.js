61e32af7b4655293b14bd77aea4bbff6ce60daae
Monica Tamaru Jones <mntamaru@gmail.com>
2018-11-04 20:31:12 +0000

    automatically backed up by learn

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -4,7 +4,7 @@ import { actors } from '../data';
 const Actors = () => {
   return (
     <div>
-      <h1>Actors Page</h1>
+      
       {actors.map((actor, i) => (
         <div key={i}>
           <h3>Name: {actor.name}</h3>
