c204bc22bc0ffed43dad309775f5734ba48b1137
Monica Tamaru Jones <mntamaru@gmail.com>
2018-11-04 20:19:11 +0000

    automatically backed up by learn

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -4,7 +4,19 @@ import { directors } from '../data';
 const Directors = () => {
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Director's Page</h1>
+      {movies.map((movie, i) => (
+        <div key={i}>
+          <h3>{movie.title}</h3>
+          <p>Time: {movie.time}</p>
+          <p>Genres:</p>
+          <ul>
+            {movie.genres.map((genre,i)=> (
+            <li key={i}>{genre}</li>
+            ))}
+          </ul>
+        </div>
+      ))}
     </div>
   );
 }
