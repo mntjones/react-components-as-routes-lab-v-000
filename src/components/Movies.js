cb27263234045cc52b837bf017b94cbe2657a9e5
Monica Tamaru Jones <mntamaru@gmail.com>
2018-11-04 20:17:08 +0000

    automatically backed up by learn

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -5,7 +5,7 @@ const Movies = () => {
   return (
     <div>
       <h1>Movies Page</h1>
-      { movies.map ((movie, i) => {
+      { movies.map ((movie, i) => 
         <div key= {i}>
           <h3>{movie.title}</h3>
           <p>Time: {movie.time} </p>
