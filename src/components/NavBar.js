a62f8ee6947d6cb846ef07fe0a63804072e95472
Monica Tamaru Jones <mntamaru@gmail.com>
2018-11-04 20:10:06 +0000

    automatically backed up by learn

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,36 +5,12 @@ const NavBar = () => {
   return (
     <div className="navbar">
       <ul>
-        <li><NavLink
-          to="/"
-          exact
-        >Home</NavLink></li>
-        <li><NavLink
-          to="/movies"
-          exact
-          style={link}
-          activeStyle={{
-            background: 'darkblue'
-          }}
-        >Movies</NavLink></li>
-        <li><NavLink
-          to="/directors"
-          exact
-          style={link}
-          activeStyle={{
-            background: 'darkblue'
-          }}
-        >Directors</NavLink></li>
-       <li><NavLink
-        to="/actors"
-        exact
-        style={link}
-        activeStyle={{
-          background: 'darkblue'
-        }}
-      >Actors</NavLink></li>
-    </ul>
-  </div>);
+        <li><NavLink to="/" exact>Home</NavLink></li>
+        <li><NavLink to="/actors" exact>Actors</NavLink></li>
+        <li><NavLink to="/directors" exact>Directors</NavLink></li>
+        <li><NavLink to="/movies" exact>Movies</NavLink></li>
+      </ul>
+    </div>);
 };
 
 export default NavBar;
